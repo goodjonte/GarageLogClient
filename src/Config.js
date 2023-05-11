0b054446ca8d49e520d9a72ecaa56299bc8bc093
Jonte Good <JG056@atcltd.atc.org.nz>
2023-05-11 19:56:25 +1200

    updated config

--- src/Config.js
+++ src/Config.js
@@ -1,5 +1,5 @@
 export default class Config {
-    static getApiUrl() {//https://garagelogapi.azure-api.net/garagelogexample/api/
-        return "https://localhost:7018/api/";
+    static getApiUrl() {
+        return "https://garagelogapi.azure-api.net/garagelogexample/api/";
     }
 }
